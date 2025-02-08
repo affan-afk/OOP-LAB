13526cf73111befb36b053c977d0ae1d0ffe1ef9
Muhammad Affan Rasheed <affanrasheed2751@gmail.com>
2025-02-08 21:25:35 +0500

    Update task4.cpp

--- LAB 02/task4.cpp
+++ LAB 02/task4.cpp
@@ -1,5 +1,6 @@
 //muhammad affan rasheed (24k-0579)
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 
@@ -15,10 +16,23 @@ using namespace std;
       getline(cin,strings[i]);
       cout<<endl;
     }
+    for (int i = 0; i < totalStrings-1; i++)
+    {
+      for (int j = 0; j < totalStrings-i-1; j++)
+      {
+         if (strings[j] > strings[j+1])
+         {
+            string temp = strings[j];
+            strings[j] = strings[j + 1];
+            strings[j + 1] = temp;
+         }
+      }
+    }
     for (int i = 0; i < totalStrings; i++)
     {
       cout<<"string "<<i+1<<": "<<strings[i];
       cout<<endl;
     }
     delete[] strings;
+    return 0;
  }
