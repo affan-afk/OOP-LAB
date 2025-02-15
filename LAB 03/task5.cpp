99e34811c0ac7171181dab04a901d3ae504c3c40
Muhammad Affan Rasheed <affanrasheed2751@gmail.com>
2025-02-15 21:55:53 +0500

    Update task5.cpp

--- LAB 03/task5.cpp
+++ LAB 03/task5.cpp
@@ -49,23 +49,44 @@ class MusicPlayer{
         {
             if (playlist[i] == song)
             {
-                playlist[i] = "";
                 cout<<"-- "<<song<< " -- is now playing!"<<endl;
                 currentlyPlayingSong = song;
                 return; 
             }
         }
+        cout<<"-- "<<song<< " -- not Found!"<<endl;
+    }
+    void displayPlaylist(){
+        int count = 1;
+        for (int i = 0; i < 100; i++)
+        {
+            if (playlist[i] != "")
+            {
+                cout<<count<<". "<<playlist[i]<<endl;
+                count++;
+            }
+        }
+        if (count == 1)
+        {
+            cout<<"Playlist Empty!"<<endl;
+        }
+        
     }
 };
 int main(){
     MusicPlayer imran_ki_playlist;
-    imran_ki_playlist.addSong("bad blood");
-    imran_ki_playlist.addSong("end of beginning");
-    imran_ki_playlist.addSong("something just like this");
-    imran_ki_playlist.addSong("something just like this");
-    imran_ki_playlist.removeSong("something just like this");
-    imran_ki_playlist.playSong("bad blood");
+    imran_ki_playlist.addSong("shape of you");
+    imran_ki_playlist.addSong("blinding lights");
+    imran_ki_playlist.addSong("levitating");
+    imran_ki_playlist.addSong("someone like you");
+    imran_ki_playlist.addSong("counting stars");
     imran_ki_playlist.playSong("bad blood");
     imran_ki_playlist.playSong("end of beginning");
+    imran_ki_playlist.addSong("shape of You");
+    imran_ki_playlist.removeSong("havana");
+    imran_ki_playlist.removeSong("despacito");
+    imran_ki_playlist.playSong("shape of you");
+    imran_ki_playlist.playSong("blinding lights");
+    imran_ki_playlist.displayPlaylist();
     return 0;
 }
