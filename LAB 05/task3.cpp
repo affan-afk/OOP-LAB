a548cceec9c728a914a884279622c34c37cc1bcd
Muhammad Affan Rasheed <affanrasheed2751@gmail.com>
2025-03-11 15:44:10 +0500

    Add files via upload

---
+++ LAB 05/task3.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+class Employee{
+ private:
+ string name;
+ string designation;
+ public:
+ Employee(string name,string desg): name(name),designation(desg){}
+void display(){
+	cout<<"emp name: "<<name<<endl;
+	cout<<"emp designation: "<<designation<<endl;
+}
+};
+class Project{
+	private:
+	string title;
+    string deadline;
+	Employee *members[10];
+	int empCount = 0;
+ public:
+	Project(string title,string dead):title(title),deadline(dead){}
+	void addEmployee(Employee *emp){
+		if (empCount < 10)
+		{
+			members[empCount++] = emp;
+		}
+	}
+	void display(){
+		cout<<"project details: "<<endl;
+		cout<<"project title: "<<title<<endl;
+		cout<<"project deadline: "<<deadline<<endl;
+		for (int i = 0; i < empCount; i++)
+		{
+			members[i]->display();
+		}
+	}
+};
+int main(){
+	Employee e1("ali","head");
+	Employee e2("ahmed","lead programmer");
+	Employee e3("mohsin","backend");
+	Project p1("game 2","9/11");
+	p1.addEmployee(&e1);
+	p1.addEmployee(&e2);
+	p1.addEmployee(&e3);
+	p1.display();
+}
---
+++ LAB 05/task4.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+class AlarmSystem{
+	private:
+	string secLvl;
+	public:
+	AlarmSystem(string lvl): secLvl(lvl){}
+	void display(){
+		cout<<"Alarm security level: "<<secLvl<<endl;
+	}
+};
+class SmartHome{
+	private:
+	AlarmSystem a1;
+	public:
+	SmartHome(AlarmSystem alarm) : a1(alarm){}
+	void display(){
+	a1.display();
+	}
+};
+int main(){
+	AlarmSystem firstalarm("high");
+	AlarmSystem secondalarm("medium");
+	AlarmSystem thirdalarm("low");
+	SmartHome s1(firstalarm);
+	SmartHome s2(secondalarm);
+	SmartHome s3(thirdalarm);
+	s1.display();
+	s2.display();s3.display();
+}
