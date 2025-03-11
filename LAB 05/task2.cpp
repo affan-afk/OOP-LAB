f88655d9cdab2c19c66e01ea787b5127cd8629a5
Muhammad Affan Rasheed <affanrasheed2751@gmail.com>
2025-03-11 15:46:43 +0500

    Update task2.cpp

--- LAB 05/task2.cpp
+++ LAB 05/task2.cpp
@@ -1,3 +1,31 @@
+//muhammad affan rasheed (24k-0579)
 #include <iostream>
 using namespace std;
- class student
+class AlarmSystem{
+	private:
+	string secLvl;
+	public:
+	AlarmSystem(string lvl): secLvl(lvl){}
+	void display(){
+		cout<<"Alarm security level: "<<secLvl<<endl;
+	}
+};
+class SmartHome{
+	private:
+	AlarmSystem a1;
+	public:
+	SmartHome(AlarmSystem alarm) : a1(alarm){}
+	void display(){
+	a1.display();
+	}
+};
+int main(){
+	AlarmSystem firstalarm("high");
+	AlarmSystem secondalarm("medium");
+	AlarmSystem thirdalarm("low");
+	SmartHome s1(firstalarm);
+	SmartHome s2(secondalarm);
+	SmartHome s3(thirdalarm);
+	s1.display();
+	s2.display();s3.display();
+}
